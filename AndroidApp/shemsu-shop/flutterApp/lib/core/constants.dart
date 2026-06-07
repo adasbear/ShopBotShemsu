39b2643d0d8119f9faa50fd0b8ada1bf8e3b60e3
Junaid <adilbefikaduararsa@gmail.com>
2026-06-07 11:33:23 +0300

    Remove AndroidApp (Kotlin + Flutter) - switching to Telegram Mini App

--- AndroidApp/shemsu-shop/flutterApp/lib/core/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String baseUrl = 'https://shopbotshemsu-1.onrender.com/';
-  static const String telegramBotToken = '8768868555:AAFriBhP04Ib9okUIbBfBiidVd55J-LVI3o';
-  static const String telegramApiBase = 'https://api.telegram.org/';
-  static const String appName = 'Shemsu Shop';
-  static const double deliveryFee = 80.0;
-}
